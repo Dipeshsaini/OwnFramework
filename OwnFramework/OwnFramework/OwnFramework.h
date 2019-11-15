02c198b99ac59b9c95b6fba14f146c92017c19e0
Dipesh Saini <mannusaini0001@gmail.com>
2019-11-15 16:00:45 +0530

    first commit

---
+++ OwnFramework/OwnFramework/OwnFramework.h
@@ -0,0 +1,19 @@
+//
+//  OwnFramework.h
+//  OwnFramework
+//
+//  Created by Rumooz Technologies on 15/11/19.
+//  Copyright © 2019 Rumooz Technologies. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OwnFramework.
+FOUNDATION_EXPORT double OwnFrameworkVersionNumber;
+
+//! Project version string for OwnFramework.
+FOUNDATION_EXPORT const unsigned char OwnFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OwnFramework/PublicHeader.h>
+
+
---
+++ OwnFramework/OwnFramework/Service.swift
@@ -0,0 +1,16 @@
+//
+//  Service.swift
+//  OwnFramework
+//
+//  Created by Rumooz Technologies on 15/11/19.
+//  Copyright © 2019 Rumooz Technologies. All rights reserved.
+//
+
+import Foundation
+class Service {
+    
+   static func testing()->String
+    {
+        return "Apna framework"
+    }
+}
